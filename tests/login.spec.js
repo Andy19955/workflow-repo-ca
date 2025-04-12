a584d3cd5051eb35fc2bc0e87fcee2f8d85d8c9a
Andy19955 <andy_19955@hotmail.com>
2025-04-13 00:22:15 +0200

    Add Playwright test for navigation and login

---
+++ tests/login.spec.js
@@ -0,0 +1,40 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("login", () => {
+  test("user can login", async ({ page }) => {
+    await page.route("*/**/auth/login", (route) =>
+      route.fulfill({
+        status: 200,
+        json: { name: "Test", email: "test@noroff.no" },
+      }),
+    );
+
+    await page.goto("/login");
+
+    await page.locator("input[name='email']").fill(process.env.TEST_USER_EMAIL);
+    await page
+      .locator("input[name='password']")
+      .fill(process.env.TEST_USER_PASSWORD);
+
+    await page.getByRole("button", { type: "submit" }).click();
+
+    await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
+  });
+
+  test("wrong password shows error", async ({ page }) => {
+    await page.goto("/login");
+
+    await page.locator("input[name='email']").fill(process.env.TEST_USER_EMAIL);
+    await page.locator("input[name='password']").fill("wrongpassword");
+
+    await page.getByRole("button", { type: "submit" }).click();
+
+    await page.waitForFunction(() => {
+      const messageContainer = document.querySelector("#message-container");
+      return (
+        messageContainer &&
+        messageContainer.textContent.includes("Invalid email or password")
+      );
+    });
+  });
+});
